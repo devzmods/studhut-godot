76d7e5e51934054b4b4aef303e1bf6b1ae14b93d
XxMeMeBigBoyxX <39273517+XxMeMeBigBoyxX@users.noreply.github.com>
2025-07-21 21:48:03 -0400

    Done for now

--- objects/GizObstacle.gd
+++ objects/GizObstacle.gd
@@ -1,6 +1,45 @@
 extends Node3D
 class_name GizObstacle
+
+#Temporary
+@export var mesh_resource: Mesh = BoxMesh.new()
+
+
 @export var Name: String
-@export var Position: Vector3
-@export var Rotation: Vector3
-@export var Scale: Vector3
+var _position: Vector3
+@export var Position: Vector3:
+	set(new_value):
+		_position = new_value
+		position = new_value
+	get:
+		return _position
+var _rotation: Vector3
+@export var Rotation: Vector3:
+	set(new_value):
+		_rotation = new_value
+		rotation = new_value
+	get:
+		return _rotation
+var _scale: Vector3
+@export var Scale: Vector3:
+	set(new_value):
+		_scale = new_value
+		scale = new_value
+	get:
+		return _scale
+	
+	
+func _ready():
+	var mesh_instance = MeshInstance3D.new()
+	
+	# 2. Assign the mesh resource to it.
+	#    This uses the exported variable, so you can change it in the editor.
+	mesh_instance.mesh = mesh_resource
+	# 4. Add the new MeshInstance3D as a child of this node.
+	#    This makes it visible and part of the scene tree.
+	add_child(mesh_instance)
+	var Tlabel = Label3D.new()
+	add_child(Tlabel)
+	Tlabel.position = Vector3(0,1,0)
+	Tlabel.text = Name
+  
